0129e65a0390315d49af7f06155e25314568fda3
Danthology96 <daniel.andres.novillo@gmail.com>
2022-01-04 18:34:23 -0500

    - add(band_name_server): Added the NodeJS server with sockets (Socket.io)

---
+++ band_name_server/sockets/socket.js
@@ -0,0 +1,15 @@
+// importación con nombre {}
+const {io} = require('../index');
+// Mensajes de sockets
+io.on('connection', client => {
+    console.log('Cliente conectado');
+    client.on('disconnect', () => { console.log('Cliente desconectado')});
+
+    client.on('mensaje',(payload)=> {
+        
+        console.log(payload);
+        io.emit('mensaje',{admin: 'Nuevo mensaje'});
+
+    });
+
+  });
